f1b02b6de1c5e5f145265c7053b9ce16ddc54e5c
Jazz Stocker-Witterick <jstocker@uoguelph.ca>
2025-09-13 14:55:00 -0400

    game screen added

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import GameScene from './scenes/GameScene.js';
 const config = {
   type: Phaser.AUTO,
   parent: 'game',
-  width: 512,
-  height: 512,
-  backgroundColor: '#fffff',
+  width: '100%',
+  height: '100%',
+  backgroundColor: '#a3a3a3',
   scene: [GameScene]
 };
 
